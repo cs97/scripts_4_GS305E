9b646d831f21a8f7b54cf1411b91e7d488d88771
cs97 <chrswr97@gmail.com>
2026-04-11 23:13:54 +0200

    Update GS305E.sh

--- GS305E.sh
+++ GS305E.sh
@@ -6,9 +6,7 @@ RAND=$(curl -s http://192.168.0.239/login.cgi | grep "id='rand'" | cut -d"'" -f4
 
 MERGED=$(paste -d '\0' <(echo -n $PASS | fold -w1) <(echo -n $RAND | fold -w1) | tr -d '\n')
 
-HASH=$(echo -n $MERGED | md5sum)
+HASH=$(echo -n $MERGED | md5sum | sed 's/..$//')
 
-PAYLOAD="password=$HASH"
-
-curl -X POST http://192.168.0.239/login.cgi -d '$PAYLOAD'
+curl -X POST http://192.168.0.239/login.cgi -d "password=$HASH"
 
