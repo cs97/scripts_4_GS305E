1b687cba8d7f7938bcb499227d5419aae1df6c47
Christian Schwarzer <christian.schwarzer@student.uni-halle.de>
2026-04-15 12:41:01 +0200

    blah

--- login-GS305E.sh
+++ login-GS305E.sh
@@ -2,11 +2,13 @@
 
 PASS="password"
 
-RAND=$(curl -s http://192.168.0.239/login.cgi | grep "id='rand'" | cut -d"'" -f4)
+RAND=$(curl -s http://192.168.0.10/login.cgi | grep "id='rand'" | cut -d"'" -f4)
+
+echo $RAND
 
 MERGED=$(paste -d '\0' <(echo -n $PASS | fold -w1) <(echo -n $RAND | fold -w1) | tr -d '\n')
 
 HASH=$(echo -n $MERGED | md5sum | sed 's/...$//')
 
-curl -X POST http://192.168.0.239/login.cgi -d "password=$HASH"
+curl -X POST http://192.168.0.10/login.cgi -d "password=$HASH" --verbose
 
